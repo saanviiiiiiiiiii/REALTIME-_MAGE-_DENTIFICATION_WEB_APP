fc088decb202a86caf4add9b9b7762a28850f53b
saanviiiiiiiiiii <71762503+saanviiiiiiiiiii@users.noreply.github.com>
2021-05-13 15:58:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+function setup()
+{
+  canvas = createCanvas(300, 300);
+  canvas.center();
+}
+ function draw()
+ {
+ }
+ Webcam.set({
+  width:350,
+  height:300,
+  image_format : 'png',
+  png_quality:90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+    
+function take_snapshot()
+{
+  Webcam.snap(function(data_uri) {
+      document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+  });
+}
+
+console.log('ml5 version:', ml5.version);
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/5mxzZHkpx/model.json',modelLoaded);
+
+function modelLoaded() {
+  console.log('Model Loaded!');
+}
